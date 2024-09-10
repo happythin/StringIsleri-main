a154bff4cd0ee607a9bc7ba2ee9e6765978941a0
happythin <happythin19@hotmail.com>
2024-09-10 21:43:14 +0300

    tip2

--- Program.cs
+++ Program.cs
@@ -1,77 +1,154 @@
 ﻿﻿// See https://aka.ms/new-console-template for more information
-// Tip Dönüşümü : C# da olan 15 adet önceden tanımlı olan tiplerin birbirlerine olan dönüşümlerini anlatan konudur.
-// C#'da 4 türlü tip dönüşümü vardır.
-/*
- 1- Convert
- 2- Cast
- 3- ToString
- 4- Parse
-*/
+
+using System.Data;
+// Kitap önerisi : Sefer Algan - Her Yönüyle C#
+#region Convert Tip Dönüşümü
+
+// Bool tipinde bir değişkeni, int tipine çevirmek
+//bool boolDegisken = false;
+//int intDegisken = Convert.ToInt32(boolDegisken);
+//Console.WriteLine(intDegisken);
+
+// bool tipinde bir değişkeni int tipine çevirebiliriz.
+// int tipine çevirilen true değeri : 1 
+// int tipine çevirilen false değeri : 0 olarak çevrilecektir.
+
+// int tipinde bir değişkeni bool tipine çevirebilir miyiz ?
+
+// int tipinde bir değişken bool tipine dönüştürülebilir.
+// int tipi, 0 değerini taşıyorsa, bool tipine dönüşten sonra false değerine dönüşür.
+// 0 harici herhangi bir değer taşıyorsa, bool tipine dönüştüren sonra true değeri taşır.
 
 
-// Convert :
+//int intDegisken = -100;
+//bool boolDegisken = Convert.ToBoolean(intDegisken);
+//Console.WriteLine(boolDegisken);
 
-// Syntax
-//int olarak dönecek değer =Convert.ToInt32(int tipine çevrilmesi için verilecek olan değer)
-/*Console.WriteLine("Lütfen bir sayıyı, ");
-string kullanicidanGelen = Console.ReadLine();
-int convertStringToInt = Convert.ToInt32(kullanicidanGelen);
-// matematiksel işlem yapalım
-int toplam = convertStringToInt +convertStringToInt;
-Console.WriteLine(toplam);
-*/
+// Char tipini, int tipine convert ile dönüştürelim
+//char charDegisken = 'Z';
+//int intDegisken = Convert.ToInt32(charDegisken);
+//Console.WriteLine(intDegisken);
+// char tipi int tipine dönüştürüldüğünde, her bir harfe karışık gelen bir rakama dönüşür. 
 
-#region Örnek 
-// ekradan alınan bir sayının tek sayımı çift sayımı olduğunu gösteren uygulama
-// tek ise true
-// çift ise false
+// Örnek : büyük A ile küçük A arasındaki sayı, 32 dir, aynı şey, büyük Z ve küçük z arasındaki sayı farkıda 32 dir.
 
-//Console.WriteLine("merhaba bir sayı giriniz");
-//string kulllanicidanAlinanString  = Console.ReadLine();
-//int intTipineDonusturme = Convert.ToInt32(kulllanicidanAlinanString);
+// int tipini char tipine çevirmek 
+//int intDegisken  = 50;
+//char charDegisken = Convert.ToChar(intDegisken);
+//Console.WriteLine(charDegisken);
 
-// bir sayının 2 ye göre modu 0 ise çift sayı, 1 ise çift sayıdır.
-//bool result =intTipineDonusturme%2!=0;
-//Console.WriteLine(result);
+// int tipi char tipine dönüştürüldüğünde, 0 ile 255 arasında farklı karakterler dönüşmektedir.
+
+
+// bir string ifadenin içerisindeki rakamları alıp, int tipine dönüştürme!!!!
+//string stringDegisken = "aaaa44433299sdsds";
+//string newString = stringDegisken.Substring(4, 8);
+//int intDegisken = Convert.ToInt32(newString);
+//Console.WriteLine(intDegisken);
+#endregion
+
+#region Parse
+
+// Convert tip dönüşümü tekrar bir hatırlayalım
+
+// C#'da kullanılan build-in (önceden tanımlı) 15 adet tanımlı tipi, birbirine çevirmek için kullanılır.
+
+
+// tip dönüşümleri hangileriydi : 
+
+// Convert 
+// Cast
+// Parse
+// ToString
+
+// Parse : Parse bir tip için özelleşmiş bir tip dönüşümüdür.
+// Parse, string tipindeki verileri, diğer tiplere çevirmek için kullanılır. Yani parse sadece string alır.
+// string değişken tipi c#da çok fazla kullanıldığı için, parse string tipine özel olarak üretilmiştir.
+// amacı, hızlı bir şekilde, string tipini farklı tiplere çevirmektir.
+// başka bir sebebide, kullanıcıdan alınan değerler string'dir. Bu yüzden de string'e özel bir dönüştürme mekanizması vardır.
+
+// Örnek ve Syntax
+
+// string tipini int tipine dönüştürelim
+//string metin = "10";
+//int intDegisken = int.Parse(metin);
+//Console.WriteLine(intDegisken);
+
+
+// string tipini char tipine dönüştürelim
+//string metin = "a";
+//char charDegisken = char.Parse(metin);
+//Console.WriteLine(charDegisken);
+
+
+// string değeri bool tipine çevirelim
+//string metin ="false";
+//bool boolDegisken = bool.Parse(metin);
+//Console.WriteLine(boolDegisken);
+
+
+// Kullanıcıdan iki farklı sayısal değer alıp, bu sayısal değerleri,
+// Ekrana toplama-çıkartma-çarpma ve bölme yapacak şekilde yazdırın, 
+//buradaki tip dönümlerinde, parse kullanın
+
+
+
 
 #endregion
 
-#region Örnek
+#region Random Konusu
+
+// Araya konu serpiştirelim
+// C# ile random bir sayı oluşturabilirsiniz
+// belirli bir aralık vererek yada vermeyerek, c#'in size bir sayı vermesini sağlayabilirsiniz
+
+// random bize, rakamsal tiplerin sınırları içerisinde random bir değer üretip verecektir.
+
+//Random rnd = new Random();
+//int intRandom = rnd.Next();
+//Console.WriteLine(intRandom);
+
+// next fonksiyonu içerisinde iki parametre girerseniz, bu iki değer araında random bir değer üretip size dönecektir.
+//Random rnd = new Random();
+//int randomInt=rnd.Next(1,5);
+//Console.WriteLine(randomInt);
+
+
+// double tipinde bir random oluşturabiliriz.
+//Random rnd =new Random();
+//double randomDouble = rnd.NextDouble();
+//Console.WriteLine(randomDouble);
 #endregion
 
-#region Örnek
-// kullanıcıdan taban ve yükseklik değerini açıp, üçgenin alanını hesaplayınız
-//Console.WriteLine("Taban değerini giriniz");
-//int taban = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Yüksellik değerini giriniz");
-//int yukseklik = Convert.ToInt32(Console.ReadLine());
-// sonuçtan ondalık dönme riskine karşı, ondalık değişkenle karşılıyoruz.
-//decimal hesap = (taban*yukseklik)/2;
-//Console.WriteLine("Alan : {0} ",hesap);
-#endregion
+#region ToString
 
-#region Örnek
-// Kullanıcıdan aldığınız, yarı çap değeri ile, dairenin alanını ve çevreni hesaplayınız()
-Console.WriteLine("Lütfen yarıçap giriniz");
-int yariCap = Convert.ToInt32(Console.ReadLine());
+// tip dönüşümlerinden 3. sırayı alan dönüşüm tipi, ToString'dir.
+// ToString tip dönüşümü, parse tip dönüşümünün tersi olarak çalışır.
 
-// Pi*r*r
-//decimal pi = 3.1415M;
-// Ancak c# da PI değerini veren bir fonksiyon var.
-double result = Math.PI*yariCap*yariCap;
-Console.WriteLine("Dairenin Alanı : {0 }",result);
+// yani parse,string tipinden diğer tiplere dönüşüm sağlarken
+// ToString'de, diğer tiplerden, string tipine dönüşüm sağlar.
+//ToString tip dönüşümü, dönüştürmek istediğimiz değişkenin sonuna ToString fonksiyonunu kullanarak uygulanır.
 
+
+// Örnek : 
+//int intDegisken =10;
+//string stringDegisken = intDegisken.ToString();
+
+// Console.WriteLine içerisinde yazdığınız string tipi dışındaki tüm değişkenleri ekrana yazdırırken ToString basar, yani siz farketmezsenizde, WriteLine metodu arka tarafta toString çağırır.
+//Console.WriteLine(10);
 
 #endregion
+// cast, bu konuyu yarına bırakacam!!!
 
 
-// Matematiksel işlemler için :
+// Parse metodu ile yapılacak örnekler
+// Örnek : Random çektirdiğiniz sayının tek sayımı çift sayımı olduğunu ekrana yazdırınız
+// tek sayı ise, true çift sayı ise false
 
+// Örnek : küçük harf olarak girilen bir char'i, int tipi kullanarak, büyük harfe çevirin
+// char tipi, int tipine dönüştürülecek, 
+//arka tarafta olan sayı bulunacak, sonrasında bulunan sayıya 32 çıkartılacak, 
+//bulunan yeni rakam tekrar string tipine çevrilecek
 
-// Bir sayısının üssünü hesaplamak için Math sınıfından faydalanıyoruz.
-Math.Pow(10,3);
-// Sonuç : 1000 
-
-// Ödev : 
-// Math sınıfına ait fonksiyonları inceleyin, 
-// Tüm fonksiyonlar için ufak örnek yapınız
+Random rnd =new Random();
+int randomDegisken =rnd.Next();
