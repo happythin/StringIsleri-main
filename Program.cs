fe846279d0991471f88f9ca0a1c6393cb3af25d5
happythin <happythin19@hotmail.com>
2024-10-19 14:09:30 +0300

    aa

--- Program.cs
+++ Program.cs
@@ -5924,7 +5924,25 @@ Console.WriteLine(p1.Soyad);
 Console.WriteLine(p1.Yas);
 
 
+Personel  p  = new Personel();
+p.Ad="Rıza";
 
+
+// pointer ile nesnenin bağlantısını keselim!!!
+p=null;
+// artık nesne ile p arasındaki bağlantı kesildi ve p isimli pointer artık yok!!!
+
+// Garbage Collector : Hiç bir pointer tarafınsan işaret edilmeyen nesneleri toplar demiştik
+// şu anda ilk bellek ihtiyacında sıraya göre pointer tarafından işaret edilmeyen nesneler toplanacka
+// ve yeni bellek alanları açılacaktır!!
+// Garbage collector : manuel olarak devreye sokulsa da manuel çalıştırmak önerilmez
+//  Garbage Collector 
+GC.Collect();
+
+// destructor : Garbage Collector'un belleği silme aşamasında, çalışan bir yapıdır!! 
+// nesne bellekten düşerken, en son olarak yapılacak işler
+// veri tabanı bağlantıları
+// dosya bağlantıları vb gibi şeyleri kapatmak için kullanılır!!
 
 
 
